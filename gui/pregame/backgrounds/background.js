48bd73e5b973975b8c749e8c189566a416bd0031
wltonlopes <wltonlopesmetal@gmail.com>
2026-02-20 14:36:54 -0300

    novos icones e atualizações para 0ad28

--- gui/pregame/backgrounds/background.js
+++ gui/pregame/backgrounds/background.js
@@ -1,39 +1,42 @@
 export const backgrounds = {
 	"bkg_01": [
 		{
-	 		"offset": (time, width) => -0.1,
+	 		"offset": () => -0.1,
 			"sprite": "background-bkg_01-1",
 			"tiling": true,
 		},
-		// {
-		// 	"offset": (time, width) => 0.04 * width * Math.cos(0.05 * time),
-		// 	"sprite": "background-bkg_01-2",
-		// 	"tiling": true,
-		// },
+		{
+			"offset": (time, width) => 0.04 * width * Math.cos(0.1 * time) + 0.01 * width * Math.cos(0.04 * time),
+			"sprite": "background-kush1-3",
+			"tiling": true
+		},
+
 	],
 	"bkg_02": [
 		{
-	 		"offset": (time, width) => -0.1,
+	 		"offset": () => -0.1,
 			"sprite": "background-bkg_02-1",
 			"tiling": true,
 		},
-		// {
-		// 	"offset": (time, width) => 0.04 * width * Math.cos(0.05 * time),
-		// 	"sprite": "background-bkg_01-2",
-		// 	"tiling": true,
-		// },
+		{
+			"offset": (time, width) => 0.04 * width * Math.cos(0.1 * time) + 0.01 * width * Math.cos(0.04 * time),
+			"sprite": "background-kush1-3",
+			"tiling": true
+		},
+
 	],
 		"bkg_03": [
 		{
-	 		"offset": (time, width) => -0.1,
+	 		"offset": () => -0.1,
 			"sprite": "background-bkg_03-1",
 			"tiling": true,
 		},
-		// {
-		// 	"offset": (time, width) => 0.04 * width * Math.cos(0.05 * time),
-		// 	"sprite": "background-bkg_01-2",
-		// 	"tiling": true,
-		// },
+		{
+			"offset": (time, width) => 0.04 * width * Math.cos(0.1 * time) + 0.01 * width * Math.cos(0.04 * time),
+			"sprite": "background-kush1-3",
+			"tiling": true
+		},
+
 	],
 
 };
