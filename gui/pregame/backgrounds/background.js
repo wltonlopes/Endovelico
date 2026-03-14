cdcdc6a6d45372d668671f3195fc70d501425e40
wltonlopes <wltonlopesmetal@gmail.com>
2026-03-14 12:01:30 -0300

    lusi background

--- gui/pregame/backgrounds/background.js
+++ gui/pregame/backgrounds/background.js
@@ -161,5 +161,23 @@ export const backgrounds = {
 		},
 
 	],
+			"bkg_10": [
+		{
+	 		"offset": () => -0.1,
+			"sprite": "background-bkg_10-1",
+			"tiling": true,
+		},
+		{
+			"offset": (time, width) => 0.04 * width * Math.cos(0.1 * time) + 0.01 * width * Math.cos(0.04 * time),
+			"sprite": "background-kush1-3",
+			"tiling": true
+		},
+		{
+			"offset": (time, width) => 0.02 * width * Math.cos(0.1 * time) + 0.01 * width * Math.cos(0.02 * time),
+			"sprite": "background-bkg_10-2",
+			"tiling": true
+		},
+
+	],
 
 };
